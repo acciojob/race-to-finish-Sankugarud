c30873c48882a84a42b1cd18206841686492aef4
sanket <95356246+Sankugarud@users.noreply.github.com>
2023-06-27 19:49:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+let a = new Promise( (resolve) => {
+	setTimeout( () => {
+		resolve("a");
+	},3000) ;
+});
+let b = new Promise( (resolve) => {
+	setTimeout( () => {
+		resolve("b");
+	},1000) ;
+})
+let c = new Promise( (resolve) => {
+	setTimeout( () => {
+		resolve("c");
+	},5000) ;
+})
+let d = new Promise( (resolve) => {
+	setTimeout( () => {
+		resolve("d");
+	},2000) ;
+})
+let e = new Promise( (resolve) => {
+	setTimeout( () => {
+		resolve("e");
+	},4000) ;
+})
+
+let x = Promise.any([a,b,c,d,e]);
+x.then( (data) => {
+	let div = document.getElementById("output");
+	div.innerHTML = data;
+})
+
+
+
